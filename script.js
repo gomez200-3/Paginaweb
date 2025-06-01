b538a48e3672f0a5e805cfbef4e7e4c40280f270
gomez200-3 <gjhesid@gmail.com>
2025-06-01 10:56:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,37 @@
-```javascript
-
-Document.addEventListener(‘DOMContentLoaded’, function() {
-
+document.addEventListener('DOMContentLoaded', function() {
     // Get the modal
+    var modal = document.getElementById("myModal");
 
-    Var modal = document.getElementById(“myModal”);
-
-
-
-    // Get the image and insert it inside the modal – use its “alt” text as a caption
-
-    Var modalImg = document.getElementById(“modalImage”);
-
-    Var emojis = document.querySelectorAll(‘#imageEmojis button.emoji’);
-
-
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var modalImg = document.getElementById("modalImage");
+    var emojis = document.querySelectorAll('#imageEmojis button.emoji');
 
     // Get the <span> element that closes the modal
-
-    Var span = document.getElementsByClassName(“close”)[0];
-
-
+    var span = document.getElementsByClassName("close")[0];
 
     // Function to open the modal
-
-    Function openModal(imageSrc) {
-
-        Modal.style.display = “block”;
-
+    function openModal(imageSrc) {
+        modal.style.display = "block";
         modalImg.src = imageSrc;
-
     }
-
-
 
     // Add click event to each emoji
-
-    Emojis.forEach(emoji => {
-
-        Emoji.addEventListener(‘click’, function() {
-
-            Var imageSrc = this.getAttribute(‘data-image’);
-
+    emojis.forEach(emoji => {
+        emoji.addEventListener('click', function() {
+            var imageSrc = this.getAttribute('data-image');
             openModal(imageSrc);
-
         });
-
     });
 
-
-
     // When the user clicks on <span> (x), close the modal
-
-    Span.onclick = function() {
-
-        Modal.style.display = “none”;
-
+    span.onclick = function() {
+        modal.style.display = "none";
     }
-
-
 
     // Close the modal if the user clicks outside of the modal
-
-    Window.onclick = function(event) {
-
-        If (event.target == modal) {
-
-            Modal.style.display = “none”;
-
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
         }
-
     }
-
 });
-
-```
-
-
-
