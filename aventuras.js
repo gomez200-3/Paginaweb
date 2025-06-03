c337d1da8c4ecc3f3883361d601faded3cfb1628
gomez200-3 <gjhesid@gmail.com>
2025-06-03 12:37:07 -0500

    Create aventuras.js

---
+++ aventuras.js
@@ -0,0 +1,22 @@
+// Navegación de los botones del menú principal
+
+// Botones
+document.getElementById('btn-musica').onclick = function() {
+  window.location = "musica.html";
+};
+document.getElementById('btn-videos').onclick = function() {
+  window.location = "videos.html";
+};
+document.getElementById('btn-galeria').onclick = function() {
+  window.location = "galeria.html";
+};
+document.getElementById('btn-notas').onclick = function() {
+  window.location = "notas.html";
+};
+document.getElementById('btn-especial').onclick = function() {
+  window.location = "especial.html";
+};
+
+document.getElementById('volver-inicio').onclick = function() {
+  window.location = "index.html";
+};
